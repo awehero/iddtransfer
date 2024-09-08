9f3179915bc42c1993cbf94b4aa0f76c8a006a84
awehero <147273991+awehero@users.noreply.github.com>
2024-09-08 19:49:40 -0400

    Update iddu.js

--- iddu.js
+++ iddu.js
@@ -1,24 +1,30 @@
-const _0x1b5c = (() => {
-    const _0x2e3d = ['P', '5', 'x', '7', 'o', 'r', '2', 'l', 'a', '9', 'u', '3', 's', '8', 't', '1', 'w', 'h', '4', 'c', 'j', 'm', 'e', 'y', '6', 'n', 'f', 'q', 'v', 'i', '0', 'k', '7', 'd', 'z', '5', '3', '8', 'p', 'w', '2', '6', 'u', 'j', '9', 'a', 'l', 'c', '1', 'r', 'n', 'e', 's', 'h', 't', 'x', 'm', 'v', 'b', 'f', 'q', '4'];
-    const _0x3c6f = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    return _0x2e3d.reduce((acc, val, idx) => (acc[val] = _0x3c6f[idx], acc), {});
-})();
+ const substitutionMap = {
+            'A': 'P', 'B': '5', 'C': 'x', 'D': '7', 'E': 'o', 'F': 'r',
+            'G': '2', 'H': 'l', 'I': 'a', 'J': '9', 'K': 'u', 'L': '3',
+            'M': 's', 'N': '8', 'O': 't', 'P': '1', 'Q': 'w', 'R': 'h',
+            'S': '4', 'T': 'c', 'U': 'j', 'V': 'm', 'W': 'e', 'X': 'y',
+            'Y': '6', 'Z': 'n', 'a': 'f', 'b': 'q', 'c': 'v', 'd': 'i',
+            'e': '0', 'f': 'k', 'g': '7', 'h': 'd', 'i': 'z', 'j': '5',
+            'k': '3', 'l': '8', 'm': 'p', 'n': 'w', 'o': '2', 'p': '6',
+            'q': 'u', 'r': 'j', 's': '9', 't': 'a', 'u': 'l', 'v': 'c',
+            'w': '1', 'x': 'r', 'y': 'n', 'z': 'e', '0': 's', '1': 'h',
+            '2': 't', '3': 'x', '4': 'm', '5': 'v', '6': 'b', '7': 'f',
+            '8': 'q', '9': '4'
+        };
 
-let _0x2a3d = JSON.stringify(n);
-let _0x5b6d = (function(_0x1d3e) {
-    let _0x2f4d = '';
-    for (let _0x3e5b = 0; _0x3e5b < _0x1d3e.length; _0x3e5b++) {
-        _0x2f4d += String.fromCharCode(_0x1d3e.charCodeAt(_0x3e5b) ^ 0x4F);
-    }
-    return btoa(_0x2f4d);
-})(_0x2a3d);
+        // Function to unscramble the text based on the substitution mapping
+        function unscramble(text) {
+            const reverseSubstitutionMap = Object.fromEntries(
+                Object.entries(substitutionMap).map(([key, value]) => [value, key])
+            );
+            return text.split('').map(char => reverseSubstitutionMap[char] || char).join('');
+        }
 
-let _0x3e7b = (function() {
-    let _0x7b2d = '';
-    for (let _0x4c3e = 0; _0x4c3e < _0x5b6d.length; _0x4c3e++) {
-        _0x7b2d += (_0x1b5c[_0x5b6d[_0x4c3e]] || _0x5b6d[_0x4c3e]);
-    }
-    return _0x7b2d;
-})();
-
-console.log(atob(_0x3e7b).split('').map(char => String.fromCharCode(char.charCodeAt(0) ^ 0x4F)).join(''));
+        // Prompt user for input
+        let userInput = prompt('Enter the text to unscramble:');
+        
+        // Unscramble the input
+        let unscrambledText = unscramble(userInput);
+        
+        // Display the unscrambled text
+        console.log('Unscrambled text: ' + unscrambledText);
