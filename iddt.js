49184441b27891a13d5dfd9c78c19a9b32944cc2
awehero <147273991+awehero@users.noreply.github.com>
2024-09-12 13:07:05 -0400

    Update iddt.js

--- iddt.js
+++ iddt.js
@@ -1,7 +1,1 @@
-const inputString = prompt('Enter your JSON string:');
-
-const transformedString = inputString
-    .replace(/{"mapId":"|","time":/g, '')  // Removes {"mapId":" and ","time":
-    .replace(/","count":/g, ',');          // Replaces ","count": with ,
-
-console.log(transformedString);
+JSON.stringify(n);
